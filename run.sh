ca538bf0e8cae24bacdca131a8641f0ba8a4c1fd
claudio9russo7 <99536010+claudio9russo7@users.noreply.github.com>
2023-04-05 16:26:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 exec CVAT_HOST = 192.168.30.13
 docker compose up -d
-  docker exec -it cvat_server bash -ic 'python3 ~/manage.py createsuperuser'
+docker exec -it cvat_server bash -ic 'python3 ~/manage.py createsuperuser'
